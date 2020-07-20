81ab41af0a2612a99cdae9acce215f4e91e4e99b
edevick <ekdevitskaya@gmail.com>
2020-07-20 13:36:19 +0300

    commited

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input a number: ");
+        int n = in.nextInt();
+        System.out.println(katalanNumbers(n));
+    }
+
+    private static long katalanNumbers(int n) {
+
+        return factorial(2 * n) / (factorial(n) * factorial(n + 1)); //get Katalan numbers
+
+    }
+
+    private static long factorial(int n) {
+        if (n == 1) return 1;
+        return factorial(n - 1) * n;
+    }
+}
